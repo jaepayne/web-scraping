e2713963f41d1a3557d574a9be6c66d83dcd42d8
Zeke Nierenberg <hampzan09@gmail.com>
2016-06-30 18:42:27 -0500

    oops

--- scraper.js
+++ scraper.js
@@ -3,66 +3,19 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const Price = require('./models');
-let numberOfResponses = 0;
 const productIds = require('./productIds.json');
 
-const processError = function(err) {
-  console.error(err);
-  process.exit(1);
-}
-
-const getPrice = function(selection) {
-  return processPrice(processSelection(selection));
-}
-
-const processSelection = function(selection) {
-  return selection.text().trim();
-};
-
-const processPrice = function(text) {
-  const start = text.indexOf('$');
-  let end = text.indexOf('.');
-  if (end === -1) {
-    while (end === -1) {
-      if (text[start + i] === ' ') {
-        end = start + i - 1;
-      }
-      i++;
-    }
-  }
-  else {
-    end = end + 3;
-  }
-  return text.slice(start, end).trim();
-}
-
-productIds.forEach(function (productId) {
-  request('https://www.amazon.com/dp/' + productId, function (error, response, body) {
-    try {
-      const $ = cheerio.load(body);
-      // console.log(body);
-      const product = {
-        amazonPrice: getPrice($('.dp-price-col .a-color-price')),
-        resellerNewPrice: getPrice($('.dp-new-col span')),
-        resellerUsedPrice: getPrice($('.dp-used-col span')),
-        productName: processSelection($('#productTitle')),
-        productId: productId
-      }
-      console.log(product);
-      Price.create(product, function(err) {
-        if(err) {
-          return processError(err);
-        }
-        numberOfResponses++;
-        if(numberOfResponses === productIds.length)  {
-          console.log('finished!')
-          process.exit(0);
-        }
-      })
-      
-    } catch(e) {
-      processError(e)
-    }
-   
-  })
-})
+/**
+ * HOW TO ADD DATA TO THE PRICE TABLE:
+ * Price.create({
+ *   productId: '0984782850',
+ *   productName: 'Towels',
+ *   amazonPrice: '$39.99',
+ *   resellerNewPrice: '$35.24',
+ *   resellerUsedPrice: '$0.66'
+ * }, function(err, price) { ... });
+ *
+ * `Price.create` is a function that takes an object. This object contains the data you want stored in MongoDB. The keys of the object you use must conform to the example above.
+ *
+ * When Node is finished inserting the document, it will call the function passed into parameter two of create.
+ */
